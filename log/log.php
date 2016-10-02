b2a39e8817488f68321794c11b11d715ee86f853
Jaume Ortolà <jaumeortola@gmail.com>
2016-10-02 19:26:47 +0200

    Update log.php

--- log/log.php
+++ log/log.php
@@ -1,10 +1,5 @@
 <?php
-
-
-$file="lt-messages.log";
-$message=date("Y-m-d H:i:s e").' '.$_GET["msg"]."\n";
-
+$file    = "lt-messages.log";
+$message = date("Y-m-d H:i:s e") . ' ' . $_GET["msg"] . "\n";
 file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
-
-
 ?>
